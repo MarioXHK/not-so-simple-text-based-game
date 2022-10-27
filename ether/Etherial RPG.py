895ef34a213b5adeb431404f85a48bcafc8eba15
MarioXHK <104402858+MarioXHK@users.noreply.github.com>
2022-10-27 14:51:45 -0600

    another change
    We're getting somewhere

--- ether/Etherial RPG.py
+++ ether/Etherial RPG.py
@@ -1,6 +1,26 @@
 import random
 import time
-def fancytext(texty, spext, waig):
+room = 0
+choice = "None"
+inventory = []
+keyitems = []
+ghostkeys = []
+seenrooms = []
+valnorth = {"n","N","north","North"}
+valsouth = {"s","S","south","South"}
+valeast = {"e","E","east","East"}
+valwest = {"w","W","west","West"}
+valinv = {"i","I","inv","Inv","inventory","Inventory"}
+valquit = {"q","Q","quit","Quit"}
+valup = {"u","U","up","Up"}
+valdown = {"d","D","down","Down"}
+valgrab = {"g","G","grab","Grab"}
+valuse = {"use","Use"}
+valhelp = {"h","H","help","Help"}
+vallook = {"l","L","look","Look"}
+valyes = {"y", "Y", "Yes", "Yeah", "Sure", "Ok", "Fine", "yes", "yeah", "sure", "ok", "fine"}
+valno = {"n", "N", "No", "Nope", "Nah", "no", "nope", "nah" "Not Today", "not today", "Not today", "not Today"}
+def fancytext(texty, spext, waig = 0):
     global haste
     divzex = 1/int(spext)
     if haste == False:
@@ -14,17 +34,77 @@ def fancytext(texty, spext, waig):
                 time.sleep(divzex*6)
             time.sleep(divzex)
     else:
-        print(texty)
+        print(texty, end = "")
     time.sleep(int(waig))
-print("Would you like to enable haste mode? (Haste mode instantly prints the text.)")
-hastething = input()
-if (hastething == "Yes" or hastething == "yes"):
+def seeroom(hostile,biome = "nul"):
+    global room
+    global seenrooms
+    if not room in seenrooms:
+        seenrooms.append(room)
+        if room == 0:
+            fancytext("You wake up with a gasp of air. Looking around you're in a very dark part of a large forest.", 40, 2)
+            fancytext("It's nearly impossible to see through this deep darkness, but you can see a light far up north.", 40, 2)
+    else:
+        if room == 0:
+            fancytext("The darkness here is near consuming", 20, 2)
+    if room == 0:
+        fancytext("Your only option you can see is going north.", 30, 2)
+            
+
+def default():
+    global choice
+    global gamenotover
+    if choice in valhelp:
+        fancytext("Glad you asked!", 30, 2)
+        fancytext("You see, to move around here, you have to type in the direction you want to go in, which can be north, south, east, west, up, or down.\n", 50, 2)
+        fancytext("If you want look around the room to see what is in it and where you can go from there, type in \"look\".\n", 50, 2)
+        fancytext("To grab something that is in the room, just type in \"grab\" and the thing will be added to your inventory.\n", 50, 2)
+        fancytext("You can't hold a lot of regular items, luckily, your pockets are very deep, so you can hold as many as items.\n", 50, 2)
+        fancytext("While you only have soo much space for regular items, you can have as much space as possible to store key items.\n", 50, 2)
+        fancytext("To see regular items and key items, type in \"inventory\" or just \"inv\".\n", 50, 1)
+        fancytext("You can use these items by typing in \"use\"", 40)
+    elif choice in valquit:
+        fancytext("Are you sure you want to quit?", 40)
+        choice = input()
+        if choice in valyes:
+            fancytext("...And so...you were stuck forever. ", 10, 2)
+            fancytext("The end :D", 40)
+            gamenotover = False
+    elif choice in valinv:
+        fancytext("Inventory: ", 40, 1)
+        print(inventory)
+        time.sleep(0.5)
+        fancytext("Key Items: ", 40, 1)
+        print(keyitems)
+    elif choice in valgrab:
+        fancytext("There is nothing to grab here.", 50)
+    elif choice in valuse:
+        if inventory == [] and keyitems == []:
+            fancytext("You have nothing to use.", 50)
+        else:
+            fancytext("What will you use? (Please type it exactly as seen in the inventory)", 50)
+            print("Inventory:", inventory)
+            print("Key Items:", keyitems)
+            choice = input()
+            if choice in inventory:
+                print("You used an inventory item")
+            elif choice in keyitems:
+                print("You used a key item")
+            else:
+                fancytext(("You don't have an item called", choice), 50, 1)
+
+
+
+print("Would you like to enable haste mode? (Haste mode instantly prints the text.)\n")
+choice = input()
+if choice in valyes:
     haste = True
 else:
     haste = False
-fancytext("Do you want to skip the intro?", 50, 2)
-introskip = input()
-if not (introskip == "Yes" or introskip == "yes"):
+fancytext("Do you want to skip the intro?\n", 50, 2)
+choice = input()
+fancytext("To look at the list of what you can do at any time, type in help.\n", 50, 2)
+if (not choice in valyes) or choice in valno:
     fancytext("Let's begin our story.\n", 20, 2)
     fancytext("Long ago, there was a world called...\n", 20, 2)
     fancytext("I forgor. :skull:\n", 10, 2)
@@ -49,3 +129,21 @@ if not (introskip == "Yes" or introskip == "yes"):
     fancytext("Oh damn.\"\n", 10, 2)
     fancytext("And with that Dr. Mo banished you into the Ether by shattering the floor below you.\n", 50, 1)
     fancytext("\"DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH!!!\"\n", 100, 2)
+    fancytext("You fall and fall, so far down. You were sure that you were dead when you hit the ground, however, when you did hit a ground, you didn't die.\n", 20, 2)
+    fancytext("However, you were knocked out cold.", 20, 2)
+    fancytext("And thus, your adventure begins, in the realm of\n", 20, 2)
+    fancytext("ETHER.", 5, 5)
+gamenotover = True
+seeroom(False)
+while gamenotover == True:
+    choice = input()
+    if room == 0:
+        if choice in valnorth:
+            room = 1
+        else:
+            default()
+        seeroom(False)
+    elif room == 1:
+        if choice in valsouth:
+            room = 0
+            seeroom(False)
